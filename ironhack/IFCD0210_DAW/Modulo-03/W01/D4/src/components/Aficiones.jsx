a204ed98805881476d0a311c28b57329214fcb9b
Charlie <charkrios@gmail.com>
2025-08-25 19:42:15 +0200

    ironhack M3 W1 D4 update (custom components enhancement)

--- ironhack/IFCD0210_DAW/Modulo-03/W01/D4/src/components/Aficiones.jsx
+++ ironhack/IFCD0210_DAW/Modulo-03/W01/D4/src/components/Aficiones.jsx
@@ -1,16 +1,25 @@
 import styles from "./Aficiones.module.css";
 
+const aficiones = [
+  "Programacion web y blockchain",
+  "Musica, tocar la guitarra y el piano",
+  "Aprendizaje de nuevos idiomas (actualmente Japones e Italiano)",
+  "Deportes de aventura (snowboard y submarinismo)",
+  "Videojuegos de simulacion y VR",
+  "Series y peliculas (ciencia ficcion)",
+];
+
 function Aficiones() {
   return (
     <div className={styles["aficiones-container"]}>
       <h2 className={styles["aficiones-title"]}>Mis Aficiones</h2>
-      <ul className={styles["aficiones-list"]}>
-        <li>Programacion web y blockchain</li>
-        <li>Musica, tocar la guitarra y el piano</li>
-        <li>Aprendizaje de nuevos idiomas (actualmente Japones e Italiano)</li>
-        <li>Deportes de aventura (snowboard y submarinismo)</li>
-        <li>Videojuegos de simulacion y VR</li>
-      </ul>
+      <div className={styles["aficiones-list"]}>
+        {aficiones.map((aficion, index) => (
+          <div key={index} className={styles["aficion-item"]}>
+            {aficion}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- ironhack/IFCD0210_DAW/Modulo-03/W01/D4/src/components/Canciones.jsx
+++ ironhack/IFCD0210_DAW/Modulo-03/W01/D4/src/components/Canciones.jsx
@@ -1,29 +1,25 @@
-import React from "react";
 import styles from "./Canciones.module.css";
 
 function Canciones({ data: canciones = [] }) {
   return (
-    <div>
+    <div className={styles["cancion-list"]}>
       <h2>Lista de Canciones</h2>
+      <div className={styles["cancion-header"]}>
+        <div className={styles["cancion-img-col"]}>Álbum</div>
+        <div className={styles["cancion-col"]}>Título</div>
+        <div className={styles["cancion-col"]}>Álbum</div>
+        <div className={styles["cancion-col"]}>Duración</div>
+        <div className={styles["cancion-col"]}>Valoración</div>
+      </div>
       {canciones.map((cancion, idx) => (
         <div key={idx} className={styles["cancion-item"]}>
-          <img
-            src={cancion?.imagenAlbum}
-            alt={cancion?.album}
-            style={{ width: 80, height: 80 }}
-          />
-          <div>
-            <strong>Título:</strong> {cancion?.titulo}
+          <div className={styles["cancion-img-col"]}>
+            <img src={cancion?.imagenAlbum} alt={cancion?.album} />
           </div>
-          <div>
-            <strong>Álbum:</strong> {cancion?.album}
-          </div>
-          <div>
-            <strong>Duración:</strong> {cancion?.duracion} segundos
-          </div>
-          <div>
-            <strong>Valoración:</strong> {cancion?.valoracion}/10
-          </div>
+          <div className={styles["cancion-col"]}>{cancion?.titulo}</div>
+          <div className={styles["cancion-col"]}>{cancion?.album}</div>
+          <div className={styles["cancion-col"]}>{cancion?.duracion} s</div>
+          <div className={styles["cancion-col"]}>{cancion?.valoracion}/10</div>
         </div>
       ))}
     </div>
